743e486f5cfe9829a40b3ffe9e4fe38cb7eaf084
SHIRLY JUSID <46875521@ort>
2022-07-07 15:53:51 -0300

    add Controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,13 +15,44 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewBag.Lista = BD.ListarEquipos();
         return View();
+    }
+
+    public IActionResult VerDetalleEquipo(int IdEquipo)
+    {
+        ViewBag.DatosEquipo = BD.VerInfoEquipo(IdEquipo);
+        ViewBag.ListaJugadores = BD.ListarJugadores(IdEquipo);
+
+        return View();
+    }
+
+    public IActionResult VerDetalleJugador(int IdJugador)
+    {
+        ViewBag.DatosJugador = BD.VerInfoJugador(IdJugador);
+
+        return View();
+    }
+
+    public IActionResult AgregarJugador(int IdEquipo)
+    {
+        ViewBag.Lista = BD.IdEquipo;
+
+         return View();
+    }
+
+     public IActionResult EliminarJugador(int IdJugador, int IdEquipo)
+     {
+        BD.EliminarPelicula(id);
+        return RedirectToAction("Index");
     }
 
     public IActionResult Privacy()
     {
         return View();
     }
+
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
