c7dff45f3af399324ce3b491fc4dacac4a5b7e23
SHIRLY JUSID <46875521@ort>
2022-07-07 14:21:32 -0300

    Models

---
+++ Models/Equipo.cs
@@ -0,0 +1,99 @@
+namespace TP6.Models;
+
+public class Equipo
+{
+     private int _idEquipo;
+    private string _nombre;
+    private string _escudo;
+    private string _camiseta;
+    private string _continente;
+    private int _copasGanadas;
+
+    public Equipo(int idEquipo, string nombres, string escudo, string camiseta, string continente, int copasGanadas)
+    {
+        _idEquipo = idEquipo;
+        _nombre = nombres;
+        _escudo = escudo;
+        _camiseta = camiseta;
+        _continente = continente;
+        _copasGanadas = copasGanadas;
+    }
+
+    public Equipo()
+    {
+        _idEquipo = 0;
+        _nombre = nombres;
+        _escudo = escudo;
+        _camiseta = camiseta;
+        _continente = continente;
+        _copasGanadas = 0;
+    }
+
+
+    public int IdEquipo
+    {
+        get
+        {
+            return _idEquipo;
+        }
+        set{
+            _idEquipo = value;
+        }
+    }
+
+     public string Nombre
+    {
+        get
+        {
+            return _nombre;
+        }
+        set{
+            _nombre = value;
+        }
+    }
+
+    public string Escudo
+    {
+        get
+        {
+            return _escudo;
+        }
+        set{
+            _escudo = value;
+        }
+    }
+
+    public string Camiseta
+    {
+        get
+        {
+            return _camiseta;
+        }
+        set{
+            _camiseta = value;
+        }
+    }
+
+    public string Continente
+    {
+        get
+        {
+            return _continente;
+        }
+        set{
+            _continente = value;
+        }
+    }
+
+    public int CopasGanadas
+    {
+        get
+        {
+            return _copasGanadas;
+        }
+        set{
+            _copasGanadas = value;
+        }
+    }
+
+}
