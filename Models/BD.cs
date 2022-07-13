28ba00ba656e6835f71c45e3c4789b1917c6199d
SHIRLY JUSID <46875521@ort>
2022-07-13 08:16:54 -0300

    add Models

---
+++ Models/BD.cs
@@ -0,0 +1,49 @@
+namespace TP6.Models;
+using System.Data.SqlClient;
+using Dapper;
+using System.Collections.Generic;
+
+public class BD {
+
+    private static string _connectionString = 
+        @"Server=A-AMI-202;DataBase=Qatar22;Trusted_Connection=True";
+
+    
+
+    public static void AgregarJugador(Jugador Jug ){
+
+        string sql = "INSERT INTO Jugadores  (IdEquipo,Nombre,FechaNacimiento) VALUES (@pNombre, pIdEquipo, pFechaNacimiento,)";
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            db.Execute(sql, new { pNombre = Jug.Nombre, pIdEquipo = Jug.IdEquipo,pFechaNacimiento = Jug.FechaNacimiento});
+        }
+       
+    }
+
+    public static void EliminarJugador(int IdJugador){
+        string sql = "DELETE FROM Jugadores WHERE IdJugador = @pIdJugador";
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            db.Execute(sql, new { pIdJugador = IdJugador });
+        }
+    }
+public static Equipo VerInfoEquipo(int IdEquipo)
+{
+    Equipo MiEquipo=null;
+       using (SqlConnection db = new SqlConnection(_connectionString)){
+        string sql = "SELECT * FROM Equipo WHERE IdEquipo=@pIdEquipo ";
+        MiEquipo= db.QueryFirstOrDefault<Equipo>(sql,new {pIdEquipo=IdEquipo});
+       
+       }
+        return MiEquipo;
+    }
+   public static Jugador VerInfoJugador(int IdJugador)
+{
+    Jugador MiJugador=null;
+       using (SqlConnection db = new SqlConnection(_connectionString)){
+        string sql = "SELECT * FROM Jugadores WHERE IdJugador=@pIdJugador ";
+       MiJugador= db.QueryFirstOrDefault<Jugador>(sql,new {pIdJugador=IdJugador});
+       
+       }
+        return MiJugador;
+    }
+
+}
